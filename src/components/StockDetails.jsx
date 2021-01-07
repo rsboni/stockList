62e5b1e80ea4652df3c9f58490b83c24ac74c6d7
Rafael Bonifacio <rsboni@gmail.com>
2021-01-07 13:45:33 -0300

    feat: component StockDetails

---
+++ src/components/StockDetails.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import ListItemText from '@material-ui/core/ListItemText'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import Chart from './Chart'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+
+
+export default React.memo((props) => {
+  const { selectedQuote } = props
+  const { stockName, quotes, variation } = selectedQuote
+
+  const variationColor = variation > 0 ? 'green' : variation === 0 ? 'gray' : 'red'
+
+  return (
+    <Container>
+      <Card margin={2}>
+        <CardContent>
+          <ListItemText primary={stockName} secondary={quotes ? `Valor: ${quotes[quotes.length - 1]}` : 0} />
+          <Chart quote={selectedQuote} />
+          <Typography align='left' style={{ color: variationColor }}>Variação: {variation ? variation.toFixed(2) : 0} %</Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  )
+})
