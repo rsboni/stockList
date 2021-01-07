d1d7f3b9b1fac905cbe0eeb4e689f52ed596552b
Rafael Bonifacio <rsboni@gmail.com>
2021-01-07 13:46:59 -0300

    refactor: App and index

--- src/App.js
+++ src/App.js
@@ -1,68 +1,54 @@
-import { w3cwebsocket as W3CWebSocket } from 'websocket'
-import { useEffect, useState, useRef } from 'react'
-
-const websocketUrl = process.env.REACT_APP_WEBSOCKET
-
-const variation = (array, num = 2) => {
-  let variationAvarage = 0
-  while (num > 1 && array.length > 2) {
-    variationAvarage = ((array[array.length - 1].value / array[array.length - 2].value) - (array[array.length - 2].value / array[array.length - 3].value)) / 2
-    num--
-  }
-  return variationAvarage * 100
-}
-
-const printList = (quotes) => {
-  const list = Object.entries(quotes).map(([key, val]) =>
-    <li key={key}>{key}: {quotes[key].valueHistory[quotes[key].valueHistory.length - 1].value} Variação: {quotes[key].variation}</li>
-  ).sort((a, b) => a.variation > b.variation)
-  return list
-}
+import React, { useEffect } from 'react'
+import { Route, Switch, useParams } from 'react-router-dom'
+import WebSocket from './client/WebSocket'
+import StockDetails from './components/StockDetails'
+import useQuotes from './hooks/useQuotes'
+import useSelectedQuote from './hooks/useSelectedQuote'
+import { orderList } from './helpers/functions'
+import StockList from './components/StockList'
+import Paper from '@material-ui/core/Paper'
+import Navbar from './components/Navbar'
 
 function App () {
-  const [quotes, setQuotes] = useState({ VAR: { valueHistory: [{ value: 0, timestamp: 0 }], variation: 0 } })
-  const [message, setMessage] = useState({ EX: 0, timestamp: 0 })
-
-  const websocket = useRef(null)
-
-  useEffect(() => {
-    websocket.current = new W3CWebSocket(websocketUrl)
-    websocket.current.onopen = () => {
-      console.log('WebSocket Client Connected')
-    }
-    websocket.current.onmessage = (message) => {
-      console.log(message.data)
-      const newMessage = JSON.parse(message.data)
-      if (newMessage.timestamp) {
-        setMessage(newMessage)
-      }
-    }
-    return () => websocket.current.close()
-  }, [])
+  const params = useParams()
+  const { quotes, addQuote } = useQuotes()
+  const { selectedQuote, setSelectedQuote, updateSelectedQuote } = useSelectedQuote()
+  const { message } = WebSocket()
 
   useEffect(() => {
-    const newQuotes = quotes
-    const action = Object.keys(message)[0]
-
-    if (!quotes.hasOwnProperty(action)) {
-      newQuotes[action] = { valueHistory: [] }
-    }
-    newQuotes[action] = { valueHistory: [...newQuotes[action].valueHistory.slice(-10), { value: message[action], timestamp: message.timestamp }], variation: variation(newQuotes[action].valueHistory) }
-
     if (message.timestamp) {
-      setQuotes(newQuotes)
+      addQuote(message)
     }
-  }, [quotes, message])
+    if (Object.keys(message)[0] === selectedQuote.stockName) {
+      updateSelectedQuote(message)
+    }
+  }, [message])
 
   return (
-    <div>
-      <ul>
-        {
-          printList(quotes)
-        }
-      </ul>
-    </div>
+    <>
+      <Navbar stockCode={selectedQuote.stockName} />
+      <Paper
+        style={{
+          padding: 10,
+          margin: 10,
+          height: '100%',
+          backgroundColor: '#fafafa'
+        }}
+        elevation={0}
+      >
+        <Route exact path='/'>
+
+          <StockList quotes={orderList(quotes)} setSelectedQuote={setSelectedQuote} />
+        </Route>
+        <Route
+          exact
+          path='/:stockCode'
+        >
+          <StockDetails selectedQuote={selectedQuote} />
+        </Route>
+      </Paper>
+    </>
   )
 }
 
-export default App
+export default React.memo(App)
