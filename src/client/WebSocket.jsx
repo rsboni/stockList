60088be0804b925ab2d3165ef5c6d70cad178af7
Rafael Bonifacio <rsboni@gmail.com>
2021-01-07 13:42:40 -0300

    refactor: separeted clients service

---
+++ src/client/WebSocket.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState, useRef } from 'react'
+import { w3cwebsocket as W3CWebSocket } from 'websocket'
+const websocketUrl = process.env.REACT_APP_WEBSOCKET
+
+function WebSocket (props) {
+  const [message, setMessage] = useState({})
+
+  const websocket = useRef(null)
+
+  useEffect(() => {
+    websocket.current = new W3CWebSocket(websocketUrl)
+    websocket.current.onopen = () => {
+      console.log('WebSocket Client Connected')
+    }
+    websocket.current.onmessage = (message) => {
+      const newMessage = JSON.parse(message.data)
+      if (newMessage.timestamp) {
+        setMessage(newMessage)
+      }
+    }
+    return () => websocket.current.close()
+  }, [])
+
+  return { message, setMessage }
+}
+
+export default WebSocket
