0bed9aef399dd32046cc9d41eca27e8d79e2ce7c
Rafael Bonifacio <rsboni@gmail.com>
2021-01-08 11:27:34 -0300

    fix: ws fallback url

--- src/client/WebSocket.jsx
+++ src/client/WebSocket.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState, useRef } from 'react'
 import { w3cwebsocket as W3CWebSocket } from 'websocket'
-const websocketUrl = process.env.REACT_APP_WEBSOCKET
+
+const websocketUrl = process.env.REACT_APP_WEBSOCKET || 'ws://localhost:8080/quotes'
 
 function WebSocket (props) {
   const [message, setMessage] = useState({})
-
   const websocket = useRef(null)
 
   useEffect(() => {
